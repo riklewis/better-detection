994a0f613817d5037ec9ead25f80f837b757aeda
Rik Lewis <rik@rik.onl>
2019-11-13 21:45:29 +0000

    Strip extra information from PHP version (eg. -1~dotdeb+8.1)

--- svn/trunk/better-detection.php
+++ svn/trunk/better-detection.php
@@ -769,7 +769,7 @@ function better_detection_show_settings() {
 }
 
 function better_detection_badge_php() {
-  $ver = phpversion();
+  $ver = better_detection_phpversion();
   $col = "critical";
   if(version_compare($ver,'7.2','>=')) {
     $col = "important";
@@ -778,6 +778,10 @@ function better_detection_badge_php() {
     $col = "success";
   }
   return 'https://img.shields.io/badge/PHP-' . $ver . '-' . $col . '.svg?logo=php&style=for-the-badge';
+}
+
+function better_detection_phpversion() {
+	return explode('-',phpversion())[0]; //trim any extra information
 }
 
 //define output for settings section
